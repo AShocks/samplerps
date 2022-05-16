8cf93061300b83c02e6ecb9525570c6b0c0be8be
AShocks <oshaughnessy.adrian@gmail.com>
2022-05-16 05:22:02 +0000

    Update code on returnGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,121 @@
-let playerScore = 0;
-let computerScore = 0;
-const playerScoreSpan = document.getElementById("player-score");
-const computerScoreSpan = document.getElementById("computer-score");
-const scoreArea_div = document.querySelector(".score-area");
-const result_p = document.querySelector(".result > p");
-const rockButton = document.getElementById("Rock");
-const paperButton = document.getElementById("Paper");
-const scissorsButton = document.getElementById("Scissors");
+document.addEventListener("DOMContentLoaded", function () {
+    let allButtons = document.getElementsByClassName("button-style");
+    for (let button of allButtons) {
+        button.addEventListener("click", function () {
+            let userAnswer = this.getAttribute("data-type");
+            playGame(userAnswer);
 
-function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
-  }
+            setTimeout(function () {
+                returnGame(userAnswer);
+            }, 4000);
+        });
+    }
+});
+
+function playGame(userAnswer) {
+
+    switch (playerChoice) {
+        case "Rock":
+            alert("Rock");
+            break;
+        case "Paper":
+            alert("Paper");
+            break;
+        case "Scissors":
+            alert("Scissors");
+            break;
+        default:
+            alert(`Game error, please try again later.`);
+            throw `Game error! Please investigate`;
+    }
+
+    
+}
   
-function win() {
-    playerScore++;
-    playerScoreSpan.innerHTML = playerScore;
-    computerScoreSpan.innerHTML = computerScore;
-}
 
-function lose() {
 
-}
+function returnGame(playerChoice) {
 
-function draw() {
+    let options = ["Rock", "Paper", "Scissors"];
+    let randomComputerAnswer = options[Math.floor(Math.random() * options.length)];
 
-}
+    if (randomComputerAnswer === "Rock" && playerChoice === "Rock") {
+        //displayComputerRock(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+            //increaseRoundNumber();
+            //increaseRoundsDrawn();
+        ;
 
-function playGame(playerChoice) {
-    const computerChoice = getComputerChoice;
-    switch (playerChoice + computerChoice) {
-        case "RockScissors":
-        case "PaperRock":
-        case "ScissorsPaper":
-          win(playerChoice, computerChoice);
-          break;
-        case "ScissorsRock":
-        case "RockPaper":
-        case "PaperScissors":
-          lose(playerChoice, computerChoice);
-          break;
-        case "RockRock":
-        case "PaperPaper":
-        case "ScissorsScissors":
-          draw(playerChoice, computerChoice);
-          break;
-      }
+    } else if (randomComputerAnswer === "Rock" && playerChoice === "Paper") {
+        //displayComputerRock(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+            //increaseRoundNumber();
+            //increaseRoundsWon();
+        ;
+
+    } else if (randomComputerAnswer === "Rock" && playerChoice === "Scissors") {
+        //displayComputerRock(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou lost!\n`);
+            //increaseRoundNumber();
+            //increaseRoundsLost();
+        ;
+
+
+    } else if (randomComputerAnswer === "Paper" && playerChoice === "Rock") {
+        //displayComputerPaper(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou lost!\n`);
+            increaseRoundNumber();
+            increaseRoundsLost();
+        ;
+
+    } else if (randomComputerAnswer === "Paper" && playerChoice === "Paper") {
+        //displayComputerPaper(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+            //increaseRoundNumber();
+            //increaseRoundsDrawn();
+        ;
+
+    } else if (randomComputerAnswer === "Paper" && playerChoice === "Scissors") {
+        //displayComputerPaper(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+            //increaseRoundNumber();
+            //increaseRoundsWon();
+        ;
+
+    } else if (randomComputerAnswer === "Scissors" && playerChoice === "Rock") {
+        //displayComputerScissors(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+            //increaseRoundNumber();
+            //increaseRoundsWon();
+        ;
+
+    } else if (randomComputerAnswer === "Scissors" && playerChoice === "Paper") {
+        //displayComputerScissors(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nYou lost!\n`);
+            //increaseRoundNumber();
+            //increaseRoundsLost();
+        ;
+
+    } else if (randomComputerAnswer === "Scissors" && playerChoice === "Scissors") {
+        //displayComputerScissors(randomComputerAnswer);
+        //setTimeout(function () {
+            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+            //increaseRoundNumber();
+            //increaseRoundsDrawn();
+        ;
+
+    } else {
+        alert(`Game error, please try again later.`);
+        throw `Game error! Please investigate`;
+    }
+
 } 
 
-function main() {
-    rockButton.addEventListener('click', function() {
-        playGame("Rock");
-    })
-
-    paperButton.addEventListener('click', function() {
-        playGame("Paper");
-    })
-
-    scissorsButton.addEventListener('click', function() {
-        playGame("Scissors");
-    })
-}
-
-main()
